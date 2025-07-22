ee328eb4809791b627e3952a5ae778ceda689b0c
Gabriel20xx <gabrielfranz31@gmail.com>
2025-07-22 14:12:57 +0200

    reorder PyAutoGUI fail-safe feature initialization

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ import cv2
 import pygetwindow as gw
 import numpy as np
 import pyautogui
-
-# Disable the PyAutoGUI fail-safe feature.
-pyautogui.FAILSAFE = False
 from pynput import keyboard
 import tkinter as tk
 from PIL import ImageGrab, Image, ImageTk
 
+# Disable the PyAutoGUI fail-safe feature.
+pyautogui.FAILSAFE = False
 
 # Setup logs directory
 LOGS_DIR = 'logs'
